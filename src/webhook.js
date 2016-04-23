714fc678f05f2b484ccfe364c06d8829bddc098b
Constantine Yurevich <yurevichcv@gmail.com>
2016-04-23 15:11:03 +0300

    initial commit

--- src/webhook.js
+++ src/webhook.js
@@ -1,7 +1,12 @@
 import debug from './utils/debug.js';
 
 async function webhook(ctx) {
-  ctx.body = 'test';
+  const req = ctx.request;
+  if (req.query['hub.verify_token'] === 'test' && req.query['hub.challenge']) {
+    ctx.body = req.query['hub.challenge'];
+  } else {
+    ctx.body = 'Error, wrong validation token';
+  }
 }
 
 export default webhook;
